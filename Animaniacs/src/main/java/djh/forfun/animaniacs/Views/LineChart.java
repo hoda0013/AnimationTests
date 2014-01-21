619e9c7fb650c0b4ce823e81014a16b93829d732
hoda0013 <dillon@coderow.com>
2014-01-21 00:07:36 -0600

    Working rounded curve. Added straight line too for comparison

--- Animaniacs/src/main/java/djh/forfun/animaniacs/Views/LineChart.java
+++ Animaniacs/src/main/java/djh/forfun/animaniacs/Views/LineChart.java
@@ -17,6 +17,8 @@ public class LineChart extends View {
 
 //    private float [] dataPoints;
     private Dynamics[] dataPoints;
+    private static final float GRAPH_SMOOTHNES = 0.15f;
+
 
     public LineChart(Context context) {
         super(context);
@@ -84,26 +86,10 @@ public class LineChart extends View {
     protected void onDraw(Canvas canvas) {
 
         float maxValue = getMax(dataPoints);
+        drawPath(canvas, maxValue);
 
-        Path path = new Path();
-        path.moveTo(getXPos(0), getYPos(dataPoints[0].getPosition(), maxValue));
 
-        for(int i = 1; i < dataPoints.length; i++){
-            path.lineTo(getXPos(i), getYPos(dataPoints[i].getPosition(), maxValue));
-        }
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(0xFF33B5E5);
-        paint.setStrokeWidth(4);
-        paint.setAntiAlias(true);
-        paint.setShadowLayer(4,2,2,0x80000000);
 
-        int left = getPaddingLeft();
-        int top = getPaddingTop();
-        int right = getWidth() - getPaddingRight();
-        int bottom = getHeight() - getPaddingBottom();
-
-        canvas.drawPath(path, paint);
     }
 
     private float getYPos(float value, float maxValue){
@@ -132,5 +118,81 @@ public class LineChart extends View {
         return max;
     }
 
+    private void drawPath(Canvas canvas, float maxValue){
+
+        Path path = drawSmoothPath(maxValue);
+        Path roughPath = drawRoughPath(maxValue);
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(0xFF33B5E5);
+        paint.setStrokeWidth(4);
+        paint.setAntiAlias(true);
+        paint.setShadowLayer(4,2,2,0x80000000);
+
+        Paint paint2 = new Paint();
+        paint2.setStyle(Paint.Style.STROKE);
+        paint2.setColor(0x99F08080);
+        paint2.setStrokeWidth(4);
+        paint2.setAntiAlias(true);
+        paint.setShadowLayer(4,2,2,0x80000000);
+
+
+        int left = getPaddingLeft();
+        int top = getPaddingTop();
+        int right = getWidth() - getPaddingRight();
+        int bottom = getHeight() - getPaddingBottom();
+
+        canvas.drawPath(path, paint);
+        canvas.drawPath(roughPath, paint2);
+    }
+
+    private Path drawRoughPath(float maxValue){
+
+        Path path = new Path();
+
+        path.moveTo(getXPos(0), getYPos(dataPoints[0].getPosition(), maxValue));
+        for(int i = 0; i < dataPoints.length; i++){
+            path.lineTo(getXPos(i), getYPos(dataPoints[i].getPosition(), maxValue));
+        }
+
+        return path;
+    }
+
+    private Path drawSmoothPath(float maxValue){
+        Path path = new Path();
+
+        path.moveTo(getXPos(0), getYPos(dataPoints[0].getPosition(), maxValue));
+
+        for (int i = 0; i < dataPoints.length - 1; i++) {
+            float thisPointX = getXPos(i);
+            float thisPointY = getYPos(dataPoints[i].getPosition(), maxValue);
+            float nextPointX = getXPos(i + 1);
+            float nextPointY = getYPos(dataPoints[si(i + 1)].getPosition(), maxValue);
+
+            float startdiffX = (nextPointX - getXPos(si(i - 1)));
+            float startdiffY = (nextPointY - getYPos(dataPoints[si(i - 1)].getPosition(), maxValue));
+            float endDiffX = (getXPos(si(i + 2)) - thisPointX);
+            float endDiffY = (getYPos(dataPoints[si(i + 2)].getPosition(), maxValue) - thisPointY);
+
+            float firstControlX = thisPointX + (GRAPH_SMOOTHNES * startdiffX);
+            float firstControlY = thisPointY + (GRAPH_SMOOTHNES * startdiffY);
+            float secondControlX = nextPointX - (GRAPH_SMOOTHNES * endDiffX);
+            float secondControlY = nextPointY - (GRAPH_SMOOTHNES * endDiffY);
+
+            path.cubicTo(firstControlX, firstControlY, secondControlX, secondControlY, nextPointX,
+                    nextPointY);
+        }
+        return path;
+    }
+
+    private int si(int i) {
+        if (i > dataPoints.length - 1) {
+            return dataPoints.length - 1;
+        } else if (i < 0) {
+            return 0;
+        }
+        return i;
+    }
 
 }
